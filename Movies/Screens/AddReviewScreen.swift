b4295e74bc6cf515f3ef51a602504ac94a140d18
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-08-03 14:42:20 +0530

    Saving Reviews

---
+++ Movies/Screens/AddReviewScreen.swift
@@ -0,0 +1,60 @@
+//
+//  AddReviewScreen.swift
+//  Movies
+//
+//  Created by Sajed Shaikh on 03/08/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct AddReviewScreen: View {
+    let movie: Movie
+    
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) private var modelContext
+    
+    @State private var subject: String = ""
+    @State private var description: String = ""
+    
+    private var isFormValid: Bool {
+        !subject.isEmptyOrWhiteSpace && !description.isEmptyOrWhiteSpace
+    }
+    
+    var body: some View {
+        Form {
+            TextField("Subject", text: $subject)
+            TextField("Description", text: $description)
+        }.navigationTitle("Add Review")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Close") {
+                        dismiss()
+                    }
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Save") {
+       
+                        let review = Review(subject: subject, body: description)
+                        review.movie = movie
+                        modelContext.insert(review)
+                        
+                        do {
+                            try modelContext.save()
+                            movie.reviews?.append(review)
+                        } catch {
+                            print(error.localizedDescription)
+                        }
+                        
+                        dismiss()
+                        
+                    }.disabled(!isFormValid)
+                }
+            }
+    }
+}
+
+//#Preview {
+//    AddReviewScreen()
+//}
--- Movies/Screens/MovieDetailScreen.swift
+++ Movies/Screens/MovieDetailScreen.swift
@@ -15,6 +15,7 @@ struct MovieDetailScreen: View {
     
     @State private var title: String = ""
     @State private var year: Int?
+    @State private var showReviewScreen: Bool = false
     
     var body: some View {
         Form {
@@ -33,10 +34,35 @@ struct MovieDetailScreen: View {
                 
                 dismiss()
             }.buttonStyle(.borderless)
+            
+            Section("Reviews") {
+                Button(action: {
+                    showReviewScreen = true
+                }, label: {
+                    Image(systemName: "plus")
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                })
+                
+                if let reviews = movie.reviews {
+                    if reviews.isEmpty {
+                        ContentUnavailableView {
+                            Text("No reviews")
+                        }
+                    } else {
+                        Text("List of reviews")
+                    }
+                }
+            }
+            
         }
         .onAppear(perform: {
             title = movie.title
             year = movie.year
+        })
+        .sheet(isPresented: $showReviewScreen, content: {
+            NavigationStack {
+                AddReviewScreen(movie: movie)
+            }
         })
     }
 }
