e6f003aa6dab19e114d82972f77ddb43ce27a49b
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-08-04 13:50:26 +0530

    Saving a New Actor

--- Movies/Screens/MovieListScreen.swift
+++ Movies/Screens/MovieListScreen.swift
@@ -10,21 +10,50 @@ import SwiftData
 
 struct MovieListScreen: View {
     
+    @Environment(\.modelContext) private var modelContext
+    
     @Query(sort:\Movie.title, order: .forward) private var movies: [Movie]
     @State private var isAddMoviePresented: Bool = false
+    @State private var isActorPresented: Bool = false
+    @State private var actorName: String = ""
+    
+    private func saveActor() {
+        let actor = Actor(name: actorName)
+        modelContext.insert(actor)
+    }
     
     var body: some View {
         MovieListView(movies: movies)
             .toolbar(content: {
-            ToolbarItem(placement: .topBarTrailing) {
-                Button("Add Movie") {
-                    isAddMoviePresented = true
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Add Actor") {
+                        isActorPresented = true
+                    }
                 }
-            }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Add Movie") {
+                        isAddMoviePresented = true
+                    }
+                }
+                
         })
         .sheet(isPresented: $isAddMoviePresented, content: {
             NavigationStack {
                 AddMovieScreen()
+            }
+        })
+        .sheet(isPresented: $isActorPresented, content: {
+            Text("Add Actor")
+                .font(.largeTitle)
+                .presentationDetents([.fraction(0.25)])
+            TextField("Actor name", text: $actorName)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            Button("Save") {
+                saveActor()
+                isActorPresented = false
             }
         })
     }
@@ -33,6 +62,6 @@ struct MovieListScreen: View {
 #Preview {
     NavigationStack {
         MovieListScreen()
-            .modelContainer(for: [Movie.self])
+            .modelContainer(for: [Movie.self, Review.self, Actor.self])
     }
 }
